50f4329487f2199fdb2fab2304158199652aeeae
william-prointernet <william@prointernet.mx>
2024-10-21 10:07:35 -0600

    Add files via upload

---
+++ controllers/usuarios.js
@@ -0,0 +1,45 @@
+const { response, request } = require('express');
+
+const usuariosGet = (req, res = response) => {
+    const { q, nombre = 'No name', apikey, page = 1, limit } = req.query;
+    res.json({
+        msg: "get API - controlador",
+        q,
+        nombre,
+        apikey,
+        page,
+        limit
+    });
+}
+const usuariosPut = (req, res = response) => {
+    const id = req.params.id;
+    res.json({
+        msg: "put API - controlador"
+    });
+}
+const usuariosPost = (req, res = response) => {
+    const { nombre, edad } = req.body;
+    res.json({
+        msg: "post API - controlador",
+        nombre,
+        edad
+    });
+}
+const usuariosDelete = (req, res = response) => {
+    res.json({
+        msg: "delete API - controlador"
+    });
+}
+const usuariosPatch = (req, res = response) => {
+    res.json({
+        msg: "patch API - controlador"
+    });
+}
+
+module.exports = {
+    usuariosGet,
+    usuariosPut,
+    usuariosPost,
+    usuariosDelete,
+    usuariosPatch
+}
